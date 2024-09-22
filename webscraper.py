5c97a2ae405e6ca201d06db3375298eb55e08eb4
Adrian030304 <adrianromanenco1@gmail.com>
2024-09-22 18:34:28 +0300

    reformat function

--- webscraper.py
+++ webscraper.py
@@ -1,19 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
-
-def book(title,author):
-    obj = {
-        "title":"",
-        "author":"",
-    }
-    obj.update(title=title,author=author)
-    return obj
-
+import psycopg2
 
 def scrape():
     page_link = "https://novelbin.com/sort/latest"
     pageToScrape = requests.get(page_link)
     soup = BeautifulSoup(pageToScrape.text,"html.parser")
+    
     titles = soup.findAll('h3',attrs={'class':'novel-title'})
     novel_titles = [title.find('a').text for title in titles]
     
@@ -22,15 +15,10 @@ def scrape():
     
     novels = []
     for novel_title, novel_author in zip(novel_titles, novel_authors):
-        novels.append(book(novel_title,novel_author))
-    print(novels)
-    
-    # novel_titles = []
-    # for title in titles:
-    #     titles_elements = title.findChildren('a',title=True)
-    #     for title_element in titles_elements:
-    #         title_text = title_element.text
-    #         novel_titles.append(title_text)
-    # print(novel_titles)
-
+        book = {
+        "title":novel_title,
+        "author":novel_author,
+    }
+        novels.append(book)
+    return novels
 print(scrape())
