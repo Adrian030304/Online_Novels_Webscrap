5f317af40b56fefe2edf159087dfb619e26f2ee6
Adrian030304 <adrianromanenco1@gmail.com>
2024-09-22 19:00:52 +0300

    multipage scrapping

--- webscraper.py
+++ webscraper.py
@@ -3,23 +3,24 @@ import requests
 import json
 
 def scrape():
-    page_link = "https://novelbin.com/sort/latest"
-    pageToScrape = requests.get(page_link)
-    soup = BeautifulSoup(pageToScrape.text,"html.parser")
-    
-    titles = soup.findAll('h3',attrs={'class':'novel-title'})
-    novel_titles = [title.find('a').text for title in titles]
-    
-    authors = soup.find_all('span',class_="author")
-    novel_authors = [author.text for author in authors]
-    
     novels = []
-    for novel_title, novel_author in zip(novel_titles, novel_authors):
-        book = {
-        "title":novel_title,
-        "author":novel_author,
-    }
-        novels.append(book)
+    page_link = "https://novelbin.com/sort/latest"
+    for i in range(1,5):
+        pageToScrape = requests.get(page_link + "?page=" + str(i))
+        soup = BeautifulSoup(pageToScrape.text,"html.parser")
+        
+        titles = soup.findAll('h3',attrs={'class':'novel-title'})
+        novel_titles = [title.find('a').text for title in titles]
+        
+        authors = soup.find_all('span',class_="author")
+        novel_authors = [author.text for author in authors]
+    
+        for novel_title, novel_author in zip(novel_titles, novel_authors):
+            book = {
+            "title":novel_title,
+            "author":novel_author,
+        }
+            novels.append(book)
     return novels
 
 with open('data.json','w') as f:
