2ec87370cdbb22566e6889e8ae47c57458a0caf1
Adrian030304 <adrianromanenco1@gmail.com>
2024-09-22 19:08:46 +0300

    testing control

--- webscraper.py
+++ webscraper.py
@@ -6,6 +6,10 @@ from time import sleep
 
 def scrape():
     novels = []
+    for i in range (0,3):
+        x = randint(2,5)
+        print(x)
+        sleep(x)
     page_link = "https://novelbin.com/sort/latest"
     for i in range(1,5):
         pageToScrape = requests.get(page_link + "?page=" + str(i))
